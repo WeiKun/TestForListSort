894d848f1d0844a14c79f09a9d36a5c156b5f50d
WeiKun <weikun_ict@163.com>
2019-03-01 23:54:06 +0800

    add shell

---
+++ ConformanceTest/run_test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Author: weikun
+# Created Time: Fri 01 Mar 2019 11:45:45 PM CST
+rm python_s
+srcpath='../cpython2.7/'
+cpath=`pwd`
+cd $srcpath
+srcpath=`pwd`
+
+#build 
+cd $srcpath
+make clean
+./configure --enable-list-sort
+make -j4
+cd $cpath
+ln -s ../cpython2.7/python python_s
+
+#test
+rm TestData.py
+./python_s GenData.py
+./python_s Test.py
